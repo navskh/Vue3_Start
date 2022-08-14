42d38c4e4cebb608f512655ec2d9f6a6c719e459
Young <navskh@naver.com>
2022-08-14 21:06:06 +0900

    init

---
+++ learn-vue3/.eslintrc.cjs
@@ -0,0 +1,27 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution');
+
+module.exports = {
+	root: true,
+	extends: [
+		'plugin:vue/vue3-essential',
+		'eslint:recommended',
+		'@vue/eslint-config-prettier',
+	],
+	rules: {
+		'prettier/prettier': [
+			'error',
+			{
+				singleQuote: true,
+				semi: true,
+				useTabs: true,
+				tabWidth: 2,
+				trailingComma: 'all',
+				printWidth: 80,
+				bracketSpacing: true,
+				arrowParens: 'avoid',
+				endOfLine: 'auto',
+			},
+		],
+	},
+};
---
+++ learn-vue3/src/lectures/TestLint.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>
+		<button v-on:click="increment">Counter : {{ counter }}</button>
+	</div>
+</template>
+
+<script>
+import { onMounted, ref } from 'vue';
+export default {
+	setup() {
+		const counter = ref(0);
+		const increment = () => counter.value++;
+
+		onMounted(() => {
+			console.log('마운트!');
+		});
+		return { counter, increment };
+	},
+};
+</script>
+
+<style></style>
